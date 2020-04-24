87f40336aa79ef744e18fa93438d959f137e2251
Charlotte B <48935021+cburggie@users.noreply.github.com>
2020-04-24 08:54:53 -0700

    Update constants.cpp
    switch back to c string constants

--- source/constants.cpp
+++ source/constants.cpp
@@ -1,11 +1,11 @@
 #include <cburggie.h>
 #include <string>
 
-const std::string cburggie::constants::window_title = "Playing With SDL2!";
+const char * cburggie::constants::window_title = "Playing With SDL2!";
 #ifdef WIN32
-const std::string cburggie::constants::font_path = "c:\\windows\\fonts\\DejaVuSansMono.ttf";
+const char * cburggie::constants::font_path = "c:\\windows\\fonts\\DejaVuSansMono.ttf";
 #else
-const std::string cburggie::constants::font_path = "/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf";
+const char * cburggie::constants::font_path = "/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf";
 #endif
 
 const int cburggie::constants::min_window_width = 100;
